82384228647e18391a1459bbd1c5b52137f27a41
haydende <haydeneastwell@outlook.com>
2020-01-23 16:27:36 +0000

    Created Netbeans project, defined operation methods and ActionListener classes
    Netbeans project:
    
    The project has been created in NetBeans 11.1 as a standard Java Application.
    
    Operation Methods:
    
    The methods for processing mathematical operations have been written. They take two double values and returns the result as a double.
    
    ActionListeners:
    
    The handlers for the ActionListeners have been defined under the JavaCalculator.ActionHandlers folder in preparation for the GUI.

---
+++ JavaCalculator/src/main/java/JavaCalculator/ActionHandlers/subtractionHandler.java
@@ -0,0 +1,17 @@
+
+package JavaCalculator.ActionHandlers;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author Hayden Eastwell
+ */
+public class subtractionHandler implements ActionListener {
+    
+    public void actionPerformed(ActionEvent ae) {
+        // TODO
+    }
+    
+}
---
+++ JavaCalculator/src/main/java/JavaCalculator/Operations.java
@@ -0,0 +1,50 @@
+
+package JavaCalculator;
+
+/**
+ * Class containing methods for mathematical operations
+ * @author Hayden Eastwell
+ */
+public class Operations {
+    
+    /**
+     * Method for addition operation
+     * @param num First number in equation (double)
+     * @param num2 Second number in equation (double)
+     * @return Result of the operation
+     */
+    public static double add(double num, double num2) {
+        return num + num2;
+    } 
+    
+    /**
+     * Method for subtraction operation
+     * @param num First number in equation (double)
+     * @param num2 Second number in equation (double)
+     * @return Result of the operation
+     */
+    public static double subtract(double num, double num2) {
+        return num - num2;
+    }
+    
+    /**
+     * Method for division operation
+     * @param num First number in equation (double)
+     * @param num2 Second number in equation (double)
+     * @return Result of the operation
+     */
+    public static double divide(double num, double num2) {
+        return num / num2;
+    }
+    
+    /**
+     * Method for multiplication operation
+     * @param num First number in equation (double)
+     * @param num2 Second number in equation (double)
+     * @return Result of the operation
+     */
+    public static double multiply(double num, double num2) {
+        return num * num2;
+    }
+    
+}
